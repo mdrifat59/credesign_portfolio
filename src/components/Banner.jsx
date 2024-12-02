ce3050f572f896515ece5cf3fe723ce941c5658c
mdrifat59 <mdrifatulislam59@gmail.com>
2024-12-02 12:58:45 +0600

    Banner section dynamily done

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,23 +1,48 @@
 import React from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios' 
 
 const Banner = () => {
+    const [subHeading, setSubHeading] = useState("")
+    const [heading, setHeading] = useState("")
+    const [paragraph, setParagrapha] = useState("")
+    const [buttonText, setButtonText] = useState("")
+    const [buttonShow, setButtonShow] = useState(false)
+    const [circuleShow, setCirculeShow] = useState(false) 
+
+    useEffect(() => {
+        async function fatchData() {
+            const data = await axios.get('http://localhost:8000/banneritem')
+            setSubHeading(data.data.subHeading)
+            setHeading(data.data.heading)
+            setParagrapha(data.data.paragraph)
+            setButtonText(data.data.buttonText)
+            setButtonShow(data.data.buttonShow)
+            setCirculeShow(data.data.circuleShow)  
+        }
+        fatchData()
+    }, [])
     return (
         <section id="banner">
             <div className="container">
                 <div className="banner-main">
                     <div className="banner-left">
+
                         <div className="banner-circal">
-                            <div className="banner-circal-1">
-                                <div className="banner-circal-2"></div>
-                            </div>
-                            <h4>I AM DESIGNER</h4>
+                            {
+                                circuleShow &&
+                                <div className="banner-circal-1">
+                                    <div className="banner-circal-2"></div>
+                                </div>
+                            }
+                            <h4>{subHeading}</h4>
                         </div>
-                        <h2>Creative Design and Web Solutions</h2>
-                        <p>Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod
-                            maxime
-                            placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem
-                            quibusdam et.</p>
-                        <button className="nav-button">Download My CV</button>
+                        <h2>{heading}</h2>
+                        <p>{paragraph}</p>
+                        {
+                            buttonShow &&
+                            <button className="nav-button">{buttonText}</button>
+                        }
                     </div>
                     <div className="banner-right">
                         <img src="./image/banner.jpg" alt="" />
