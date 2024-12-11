d2b84e87eb9f029979c74e71e464f8fa121e6295
mdrifat59 <mdrifatulislam59@gmail.com>
2024-12-12 00:24:30 +0600

    Experiance section api data emplement done

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,97 +1,102 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Experience = () => {
+    const [title, setTitle] = useState('')
+    const [heading, setHeading] = useState('')
+    const [showCircule, setShowCircule] = useState('')
+    const [education, setEducation] = useState([])
+    const [soft, setSoft] = useState([])
+    const [experiance, setExperiance] = useState([])
+    useEffect(() => {
+        async function fatchData() {
+            const { data } = await axios.get('http://localhost:8000/resumeitem')
+            setTitle(data.title)
+            setHeading(data.heading)
+            setShowCircule(data.showCircule)
+        }
+        async function fatchEduData() {
+            const { data } = await axios.get('http://localhost:8000/resumeeducationitem')
+            setEducation(data)
+        }
+        async function fatchsoftData() {
+            const { data } = await axios.get('http://localhost:8000/resumesoftitem')
+            setSoft(data)
+
+        }
+        async function fatchexperianceData() {
+            const { data } = await axios.get('http://localhost:8000/resumeexperianceitem')
+            setExperiance(data)
+
+        }
+        fatchexperianceData()
+        fatchsoftData()
+        fatchEduData()
+        fatchData()
+    }, [])
     return (
         <section id="experience">
             <div className="container">
                 <div className="heading">
                     <div className="banner-circal">
-                        <div className="banner-circal-1">
-                            <div className="banner-circal-2"></div>
-                        </div>
-                        <h4>My Resume</h4>
+                        {
+                            showCircule &&
+                            <div className="banner-circal-1">
+                                <div className="banner-circal-2"></div>
+                            </div>
+                        }
+                        <h4>{title}</h4>
                     </div>
-                    <h2>10+ YEARS OF EXPERIENCE</h2>
+                    <h2>{heading}</h2>
                 </div>
                 <div className="exparience-content">
                     <h4>Education</h4>
                     <div className="exparience-subcontent">
-
-                        <div className="exparience-box">
-                            <div className="exparience-box-content">
-                                <h4>BSc in Computer Science</h4>
-                                <h5>University of ULAV (2018 - 2022)</h5>
-                                <p>Hen our power of choice is untrammelled and when nothing prevents our being able</p>
-                            </div>
-                        </div>
-                        <div className="exparience-box">
-                            <div className="exparience-box-content">
-                                <h4>BSc in Computer Science</h4>
-                                <h5>University of ULAV (2018 - 2022)</h5>
-                                <p>Hen our power of choice is untrammelled and when nothing prevents our being able</p>
-                            </div>
-                        </div>
-                        <div className="exparience-box">
-                            <div className="exparience-box-content">
-                                <h4>BSc in Computer Science</h4>
-                                <h5>University of ULAV (2018 - 2022)</h5>
-                                <p>Hen our power of choice is untrammelled and when nothing prevents our being able</p>
-                            </div>
-                        </div>
+                        {
+                            education.map((item) => (
+                                <div className="exparience-box">
+                                    <div className="exparience-box-content">
+                                        <h4>{item.eduHeading}</h4>
+                                        <h5>{item.eduTitle}</h5>
+                                        <p>{item.eduDes}</p>
+                                    </div>
+                                </div>
+                            ))
+                        }
                     </div>
                 </div>
                 <div className="exparience-content">
                     <h4>Software Skills</h4>
                     <div className="exparience-subcontent">
+                        {
+                            soft.map((item) => (
 
-                        <div className="exparience-box">
-                            <div className="exparience-box-content">
-                                <h4>UI/UX Design</h4>
-                                <h5>University of ULAV (2018 - 2022)</h5>
-                                <p>Hen our power of choice is untrammelled and when nothing prevents our being able</p>
-                            </div>
-                        </div>
-                        <div className="exparience-box">
-                            <div className="exparience-box-content">
-                                <h4>Web Developer </h4>
-                                <h5>University of ULAV (2018 - 2022)</h5>
-                                <p>Hen our power of choice is untrammelled and when nothing prevents our being able</p>
-                            </div>
-                        </div>
-                        <div className="exparience-box">
-                            <div className="exparience-box-content">
-                                <h4>App Development</h4>
-                                <h5>University of ULAV (2018 - 2022)</h5>
-                                <p>Hen our power of choice is untrammelled and when nothing prevents our being able</p>
-                            </div>
-                        </div>
+                                <div className="exparience-box">
+                                    <div className="exparience-box-content">
+                                        <h4>{item.sofHeading}</h4>
+                                        <h5>{item.sofTitle}</h5>
+                                        <p>{item.sofDes}</p>
+                                    </div>
+                                </div>
+                            ))
+                        }
                     </div>
                 </div>
                 <div className="exparience-content">
                     <h4>Experience</h4>
                     <div className="exparience-subcontent">
+                        {
+                            experiance.map((item) => (
+                                <div className="exparience-box">
+                                    <div className="exparience-box-content">
+                                        <h4>{item.expHeading}</h4>
+                                        <h5>{item.expTitle}</h5>
+                                        <p>{item.expDes}</p>
+                                    </div>
+                                </div>
+                            ))
+                        }
 
-                        <div className="exparience-box">
-                            <div className="exparience-box-content">
-                                <h4>Web Developer & Trainer</h4>
-                                <h5>University of ULAV (2018 - 2022)</h5>
-                                <p>Hen our power of choice is untrammelled and when nothing prevents our being able</p>
-                            </div>
-                        </div>
-                        <div className="exparience-box">
-                            <div className="exparience-box-content">
-                                <h4>Front-end Developer</h4>
-                                <h5>University of ULAV (2018 - 2022)</h5>
-                                <p>Hen our power of choice is untrammelled and when nothing prevents our being able</p>
-                            </div>
-                        </div>
-                        <div className="exparience-box">
-                            <div className="exparience-box-content">
-                                <h4>UI/UX Designer</h4>
-                                <h5>University of ULAV (2018 - 2022)</h5>
-                                <p>Hen our power of choice is untrammelled and when nothing prevents our being able</p>
-                            </div>
-                        </div>
                     </div>
                 </div>
             </div>
