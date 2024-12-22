a47ac564ee6786111c310788c9b7f2dff493a84a
mdrifat59 <mdrifatulislam59@gmail.com>
2024-12-22 11:49:46 +0600

    Testimonial api add done

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const Testimonial = () => {
+    const [testimonial, setTestimonial] = useState([])
+    useEffect(() => {
+        async function fatchData() {
+            const { data } = await axios.get('http://localhost:8000/testimonialitem')
+            setTestimonial(data)
+        }
+        fatchData()
+    }, [])
     return (
         <section id="testimonial">
             <div className="container">
@@ -14,41 +23,22 @@ const Testimonial = () => {
                     <h2>Client Feedback</h2>
                 </div>
                 <div className="testimonial-content">
-                    <div className="testimonial-box">
-                        <div className="testimonial-heading">
-                            <img src="./image/testimonial.png" alt="" />
-                            <div>
-                                <h4>Siam Talukder</h4>
-                                <h5>CTO, Xyz Group</h5>
-                            </div>
-                        </div>
-                        <p>“Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of
-                            classNameical Latin literature fr45 BC, mak it over 2000 years old.</p>
-                    </div>
-                    <div className="testimonial-box">
-                        <div className="testimonial-heading">
-                            <img src="./image/testimonial2.png" alt="" />
-                            <div>
-                                <h4>ABM Shawon Islam</h4>
-                                <h5>CEO, Abc Agency</h5>
-                            </div>
-                        </div>
-                        <p>“Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of
-                            classNameical Latin literature fr45 BC, mak it over 2000 years old.</p>
-                    </div>
-                    <div className="testimonial-box">
-                        <div className="testimonial-heading">
-                            <img src="./image/testimonial3.png" alt="" />
-                            <div>
-                                <h4>Azmir Uddin Alif</h4>
-                                <h5>Director, Techso</h5>
-                            </div>
-                        </div>
-                        <p>“Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of
-                            classNameical Latin literature fr45 BC, mak it over 2000 years old.</p>
-                    </div>
-                </div>
+                    {
+                        testimonial.map((item, index) => (
 
+                            <div key={index} className="testimonial-box">
+                                <div className="testimonial-heading">
+                                    <img src={`http://localhost:8000/${item.image}`} alt="" />
+                                    <div>
+                                        <h4>{item.heading}</h4>
+                                        <h5>{item.title}</h5>
+                                    </div>
+                                </div>
+                                <p>{item.description}</p>
+                            </div>
+                        ))
+                    }
+                </div>
             </div>
         </section>
     )
