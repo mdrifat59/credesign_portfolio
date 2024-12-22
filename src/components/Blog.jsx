1a791ad938e75c055c7a514884ef79771be7b734
mdrifat59 <mdrifatulislam59@gmail.com>
2024-12-22 23:04:32 +0600

    Blog api add done

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const Blog = () => {
+    const [blog, setBlog] = useState([])
+    useEffect(() => {
+        async function fatchData() {
+            const { data } = await axios.get('http://localhost:8000/blogitem')
+            setBlog(data)
+        }
+        fatchData()
+    }, [])
     return (
         <section id="blog">
             <div className="container">
@@ -14,48 +23,24 @@ const Blog = () => {
                     <h2>LATEST BLOG</h2>
                 </div>
                 <div className="blog-content">
-                    <div className="blog-box">
-                        <img src="./image/blog.png" alt="" />
-                        <div className="blog-heading">
-                            <p>20 January, 2023</p>
-                            <h2>Become a UX/UI Designer With Career Foundry.</h2>
-                            <div className="blog-footer">
-                                <h4>Read More</h4>
-                                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
-                                    fill="#FF6B00">
-                                    <path d="M647-440H160v-80h487L423-744l57-56 320 320-320 320-57-56 224-224Z" />
-                                </svg>
+                    {
+                        blog.map((item, index) => (
+                            <div key={index} className="blog-box">
+                                <img src={`http://localhost:8000/${item.image}`} alt="" />
+                                <div className="blog-heading">
+                                    <p>{item.date}</p>
+                                    <h2>{item.description}</h2>
+                                    <div className="blog-footer">
+                                        <h4>Read More</h4>
+                                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
+                                            fill="#FF6B00">
+                                            <path d="M647-440H160v-80h487L423-744l57-56 320 320-320 320-57-56 224-224Z" />
+                                        </svg>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    </div>
-                    <div className="blog-box">
-                        <img src="./image/blog2.png" alt="" />
-                        <div className="blog-heading">
-                            <p>15 January, 2023</p>
-                            <h2>The Best App Development For Your Business Plan.</h2>
-                            <div className="blog-footer">
-                                <h4>Read More</h4>
-                                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
-                                    fill="#FF6B00">
-                                    <path d="M647-440H160v-80h487L423-744l57-56 320 320-320 320-57-56 224-224Z" />
-                                </svg>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="blog-box">
-                        <img src="./image/blog3.png" alt="" />
-                        <div className="blog-heading">
-                            <p>8 January, 2023</p>
-                            <h2>The Best Portfolio For Agency Design Thinking</h2>
-                            <div className="blog-footer">
-                                <h4>Read More</h4>
-                                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
-                                    fill="#FF6B00">
-                                    <path d="M647-440H160v-80h487L423-744l57-56 320 320-320 320-57-56 224-224Z" />
-                                </svg>
-                            </div>
-                        </div>
-                    </div>
+                        ))
+                    }
                 </div>
             </div>
         </section>
