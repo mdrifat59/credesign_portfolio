4447e204d970fc5a858beca9103d84e2afeda19e
mdrifat59 <mdrifatulislam59@gmail.com>
2024-12-23 14:18:15 +0600

    Footer section api connected

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,32 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const Footer = () => {
+    const [description, setDescription] = useState('')
+    const [email, setEmail] = useState('')
+    const [explore, setExplore] = useState([])
+    const [service, setService] = useState([])
+    const [address, setAddress] = useState('')
+    const [phone, setPhone] = useState('')
+
+    useEffect(() => {
+        async function fatchData() {
+            const { data } = await axios.get('http://localhost:8000/footeritem')
+            setAddress(data.address)
+            setDescription(data.description)
+            setEmail(data.email)
+            setPhone(data.phone)
+            setExplore(data.explore.split(","))
+            setService(data.service.split(','))
+        }
+        fatchData()
+    }, [])
     return (
         <section id="footer">
             <div className="container">
                 <div className="footer-content">
                     <div className="footer-fast">
                         <img src="./image/footer.png" alt="" />
-                        <p>At vero eos et accusamus et iusto odio dign ducimus qui blanditiis praesentium volup deleniti
-                            atque corrupti quos dolores et molestias excepturi sint occaecati </p>
-                        <h4>credesign@gmail.com</h4>
+                        <p>{description}</p>
+                        <h4>{email}</h4>
                     </div>
                     <div className="footer-second">
                         <h2>Explore Link</h2>
                         <ul>
-                            <li><a href="#">About</a></li>
-                            <li><a href="#">Resume</a></li>
-                            <li><a href="#">Portfolio</a></li>
-                            <li><a href="#">Blog</a></li>
+                            {
+                                explore.map((item, index) => (
+                                    <li key={index}><a href="#">{item}</a></li>
+                                ))
+                            }
                         </ul>
                     </div>
                     <div className="footer-second">
                         <h2>My Services</h2>
                         <ul>
-                            <li><a href="#">UI/UX Design</a></li>
-                            <li><a href="#">Mobile App</a></li>
-                            <li><a href="#">Graphics Design</a></li>
-                            <li><a href="#">Web Developer</a></li>
+                            {
+                                service.map((item, index) => (
+                                    <li key={index}><a href="#">{item}</a></li>
+                                ))
+                            }
                         </ul>
                     </div>
                     <div className="footer-second">
@@ -50,13 +71,13 @@ const Footer = () => {
                                 <div>
                                     <img src="./image/footerimg.png" alt="" />
                                 </div>
-                                <p>202 Dog Hill Lane Beloit, KS 67420</p>
+                                <p>{address}</p>
                             </li>
                             <li className="address">
                                 <div>
                                     <img src="./image/footerimg2.png" alt="" />
                                 </div>
-                                <p>1-800-915-6270</p>
+                                <p>{phone}</p>
                             </li>
                         </ul>
                     </div>
